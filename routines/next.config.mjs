0760c88f78ca97a092252c2d7182a913198ddb8c
Rouven <rouven@routines-app.com>
2024-06-19 08:57:23 +0200

    bugfix: fixed host in config

--- routines/next.config.mjs
+++ routines/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     FIREBASE_API_KEY: 'AIzaSyBHjLlW_J5sFUYqtUDKgPrQUsBOvgpBg8E',
     FIREBASE_AUTH_DOMAIN: 'routines-dev-70ed4.firebaseapp.com',
-    ROUTINES_HOST: "https://api.routines-app.com"
+    ROUTINES_HOST: "https://routines-api-dev-8ovqpekk.ew.gateway.dev"
   },
 
   reactStrictMode: true,
